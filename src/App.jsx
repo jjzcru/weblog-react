7ec7a3176b55a08b686bf9d3911b58b8e1379aba
Jose J. Cruz <jjzcru@gmail.com>
2021-11-17 17:30:28 -0500

    Overwrite bootstrap properties

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,31 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { AppContext } from './services/AppContext';
 import ReloadPrompt from './ReloadPrompt';
 
-import {Main, SelectedCategory, SelectedPost} from './pages/main';
-import {Home} from './pages/home';
+import { Main, SelectedCategory, SelectedPost } from './pages/main';
+import { Home } from './pages/home';
 
 export class App extends Component {
 	state = {};
-	componentDidMount() {
-		
-	}
+	componentDidMount() {}
 
 	render() {
 		return (
-			<AppContext.Provider
-				value={{
-				}}
-			>
+			<AppContext.Provider value={{}}>
 				<main className={styles['app']}>
 					<Router>
 						<Switch>
 							<Route exact path="/" component={Home} />
 							<Route exact path="/categories" component={Main} />
-							<Route exact path="/categories/:categoryId" component={SelectedCategory} />
-							<Route exact path="/categories/:categoryId/:postId" component={SelectedPost} />
+							<Route
+								exact
+								path="/categories/:categoryId"
+								component={SelectedCategory}
+							/>
+							<Route
+								exact
+								path="/categories/:categoryId/:postId"
+								component={SelectedPost}
+							/>
 						</Switch>
 					</Router>
 				</main>
@@ -34,4 +37,3 @@ export class App extends Component {
 		);
 	}
 }
-
