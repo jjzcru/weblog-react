6da9c5fa7f461aa4b88cded467d3eb6280162a71
Jose J. Cruz <jjzcru@gmail.com>
2021-11-21 22:24:54 -0500

    feat: :sparkles: Add logout

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -17,63 +17,58 @@ export function Main() {
 
 export function SelectedCategory() {
 	const { categoryId } = useParams();
-	const {api} = useContext(AppContext);
+	const { api } = useContext(AppContext);
 	const [category, setCategory] = useState(null);
-	const [categories, setCategories] = useState([])
-	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
 		api.getCategories()
 			.then((categories) => {
-				setCategories(categories);
 				for (const category of categories) {
 					if (category.id === categoryId) {
 						setCategory(category);
 						break;
 					}
 				}
-				return api.getPostsFromCategory(categoryId);
 			})
-			.then(setPosts)
 			.catch(alert);
-	}, []);
+	}, [categoryId]);
 	return (
 		<main className={styles['posts-view']}>
-			<Categories categories={categories} />
-			<Posts posts={posts} category={category} />
+			<Categories />
+			<Posts category={category} />
 			<Empty message={'Please select a post'} />
 		</main>
 	);
 }
 
 export function SelectedPost() {
-	const { categoryId, postId } = useParams();
-	let category = null;
-	for (const c of categories) {
-		if (`${c.id}` === `${categoryId}`) {
-			category = c;
-			break;
-		}
-	}
+	const { categoryId } = useParams();
+	const { api } = useContext(AppContext);
+	const [category, setCategory] = useState(null);
 
-	let post = null;
-	for (const p of category.posts) {
-		if (`${p.id}` === `${postId}`) {
-			post = p;
-			break;
-		}
-	}
+	useEffect(() => {
+		api.getCategories()
+			.then((categories) => {
+				for (const category of categories) {
+					if (category.id === categoryId) {
+						setCategory(category);
+						break;
+					}
+				}
+			})
+			.catch(alert);
+	}, []);
 
 	return (
 		<main className={styles['post-view']}>
-			<Categories categories={categories} />
+			<Categories />
 			<Posts category={category} />
-			<Post post={post} />
+			<Post />
 		</main>
 	);
 }
-function Categories({ img, username }) {
-	const { api } = useContext(AppContext);
+function Categories() {
+	const { api, me } = useContext(AppContext);
 	const [categories, setCategories] = useState([]);
 	useEffect(() => {
 		api.getCategories().then(setCategories).catch(alert);
@@ -84,7 +79,16 @@ function Categories({ img, username }) {
 				<div>
 					<img />
 				</div>
-				<div>User name</div>
+				<div>{me.name}</div>
+				<div>
+					<button onClick={()=> {
+						localStorage.removeItem('token');
+						localStorage.removeItem('expiredAt');
+						window.location.href = '/';
+					}}>
+						Log out
+					</button>
+				</div>
 			</header>
 			<ul>
 				{categories.map((category) => {
@@ -100,11 +104,30 @@ function Categories({ img, username }) {
 	);
 }
 
-function Posts({ category, posts }) {
+function Posts({ category }) {
+	const { categoryId } = useParams();
+	const { api } = useContext(AppContext);
 	const [isNewPostOpen, setIsNewPostOpen] = useState(false);
-	if(!category) {
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		if (category) {
+			api.getPostsFromCategory(categoryId).then(setPosts).catch(alert);
+		}
+	}, [category]);
+
+	if (!category) {
 		return null;
 	}
+
+	const onPostCreated = () => {
+		setIsNewPostOpen(false);
+		api.getPostsFromCategory(categoryId)
+			.then((response) => {
+				setPosts(response);
+			})
+			.catch(alert);
+	};
+
 	const { id, name } = category;
 	let component = (
 		<ul>
@@ -133,13 +156,19 @@ function Posts({ category, posts }) {
 				</div>
 				<div>{name}</div>
 				<div>
-					<button onClick={() => {
-						setIsNewPostOpen(true);
-					}}>Add </button>
+					<button
+						onClick={() => {
+							setIsNewPostOpen(true);
+						}}
+					>
+						Add{' '}
+					</button>
 				</div>
 			</header>
 			<div>
 				<NewPost
+					category={category}
+					onCreate={onPostCreated}
 					isOpen={isNewPostOpen}
 					onClose={() => {
 						setIsNewPostOpen(false);
@@ -151,10 +180,29 @@ function Posts({ category, posts }) {
 	);
 }
 
-function NewPost({ onClose, isOpen }) {
+function NewPost({ onClose, isOpen, onCreate, category }) {
+	const { api } = useContext(AppContext);
+	const [title, setTitle] = useState('');
+	const [content, setContent] = useState('');
 	const onSubmit = (e) => {
 		e.preventDefault();
 		console.log(`I add a new post`);
+		console.log({
+			title,
+			content,
+		});
+		api.createPost({
+			title,
+			description: '',
+			content,
+			categoryId: category.id,
+		})
+			.then(() => {
+				setTitle('');
+				setContent('');
+				onCreate();
+			})
+			.catch(alert);
 	};
 	return (
 		<Modal
@@ -169,20 +217,32 @@ function NewPost({ onClose, isOpen }) {
 					type="text"
 					className={styles['post-title']}
 					placeholder={'Title'}
+					value={title}
+					onChange={(e) => {
+						e.preventDefault();
+						setTitle(e.target.value);
+					}}
 				/>
 				<br />
 				<br />
 				<textarea
 					placeholder={'Write new blog post...'}
 					className={styles['post-textarea']}
+					value={content}
+					onChange={(e) => {
+						e.preventDefault();
+						setContent(e.target.value);
+					}}
 				/>
 				<br />
 				<br />
-				<button onClick={onClose} className={styles['post-button']}>
-					Post
-				</button>
+				<button className={styles['post-button']}>Post</button>
 			</form>
-			<button onClick={onClose} className={styles['cancel-button']}>
+			<button
+				type={'button'}
+				onClick={onClose}
+				className={styles['cancel-button']}
+			>
 				Cancel
 			</button>
 		</Modal>
@@ -197,91 +257,198 @@ function EmptyPosts() {
 	);
 }
 
-function Post({ post }) {
-	const { title, description, comments } = post;
-	const [isEditPostOpen, setIsEditPostOpen] = React.useState(false);
+function Post() {
+	const { categoryId, postId } = useParams();
+	const { api, me } = useContext(AppContext);
+	const [comment, setComment] = useState('');
+	const [post, setPost] = useState(null);
+	const [isEditPostOpen, setIsEditPostOpen] = useState(false);
 
-	function editPost() {
+	useEffect(() => {
+		api.getPost(postId)
+			.then((post) => {
+				setPost(post);
+				setComment('');
+			})
+			.catch(alert);
+	}, [postId]);
+
+	const onClickEditPost = () => {
 		setIsEditPostOpen(true);
+	};
+
+	const onSubmitComment = (e) => {
+		e.preventDefault();
+		api.addComment({ postId, content: comment })
+			.then(() => api.getPost(postId))
+			.then((post) => {
+				setPost(post);
+				setComment('');
+			})
+			.catch(alert);
+	};
+
+	const onDeleteComment = (id) => {
+		api.deleteComment(id)
+			.then(() => api.getPost(postId))
+			.then((post) => {
+				setPost(post);
+			})
+			.catch(alert);
+	};
+
+	const onClickDelete = async () => {
+		if (confirm(`Are you sure you want to delete: "${post.title}"`)) {
+			api.deletePost(post.id)
+				.then(() => {
+					window.location.href = `/categories/${categoryId}`;
+				})
+				.catch(alert);
+		}
+	};
+
+	if (!post) {
+		return null;
 	}
 
-	function closeEditPost() {
-		setIsEditPostOpen(false);
+	const { title, description, content, comments, user } = post;
+
+	let editButton = null;
+	let deleteButton = null;
+	if (me.id === user?.id) {
+		editButton = (
+			<button onClick={onClickEditPost} className={styles['edit-button']}>
+				Edit
+			</button>
+		);
+		deleteButton = (
+			<button onClick={onClickDelete} className={styles['edit-button']}>
+				Delete
+			</button>
+		);
 	}
 
 	return (
-		<section className={styles['post']}>
-			<header>
-				<div>{title}</div>
-			</header>
-			<section>
-				<h2>{title}</h2>
-				<div>
-					<button
-						onClick={editPost}
-						className={styles['edit-button']}
+		<>
+			<section className={styles['post']}>
+				<header>
+					<div>{deleteButton}</div>
+					<div>{title}</div>
+					<div>{editButton}</div>
+				</header>
+				<section>
+					<h2>{title}</h2>
+					{description ? (
+						<div>
+							<small>{description}</small>
+						</div>
+					) : null}
+					<p dangerouslySetInnerHTML={{ __html: content }}></p>
+					<form
+						onSubmit={onSubmitComment}
+						className={styles['comment-container']}
 					>
-						Edit Post
-					</button>
-					<Modal
-						isOpen={isEditPostOpen}
-						onRequestClose={closeEditPost}
-						className={styles['new-post-modal']}
-						contentLabel="New Post Modal"
-					>
-						<h2>Editing "{title}"</h2>
-						<form>
-							<input
-								type="text"
-								className={styles['post-title']}
-								value={title}
-							/>
-							<br />
-							<br />
-							<textarea
-								placeholder={'Write new blog post...'}
-								className={styles['post-textarea']}
-							>
-								{description}
-							</textarea>
-							<br />
-							<br />
-							<button
-								onClick={closeEditPost}
-								className={styles['post-button']}
-							>
-								Save
-							</button>
-						</form>
-						<button
-							onClick={closeEditPost}
-							className={styles['cancel-button']}
-						>
-							Cancel
-						</button>
-					</Modal>
-				</div>
-				<p>{description}</p>
-				<form>
-					<textarea placeholder={'Comment'} />
-				</form>
+						<textarea
+							value={comment}
+							onChange={(e) => {
+								setComment(e.target.value);
+							}}
+							placeholder={'Comment'}
+						/>
+						<div>
+							<button>Send</button>
+						</div>
+					</form>
+				</section>
+				<ul className={styles['comments']}>
+					{comments && comments.length
+						? comments.map((comment, i) => {
+								const { content, id } = comment;
+								let deleteCommentBtn = null;
+								if (me.id === comment.user.id) {
+									deleteCommentBtn = (
+										<button
+											onClick={() => {
+												onDeleteComment(id);
+											}}
+										>
+											Delete
+										</button>
+									);
+								}
+								return (
+									<li key={i}>
+										<div>
+											<img />
+										</div>
+										<span>{content}</span>
+										<div>{deleteCommentBtn}</div>
+									</li>
+								);
+						  })
+						: null}
+				</ul>
 			</section>
-			<ul className={styles['comments']}>
-				{comments && comments.length
-					? comments.map((comment, i) => {
-							const { content, id, userImg } = comment;
-							return (
-								<li key={i}>
-									<div>
-										<img />
-									</div>
-									<span>{content}</span>
-								</li>
-							);
-					  })
-					: null}
-			</ul>
-		</section>
+			<EditPost
+				isOpen={isEditPostOpen}
+				post={post}
+				onClose={() => {
+					setIsEditPostOpen(false);
+				}}
+			/>
+		</>
+	);
+}
+
+function EditPost({ post, isOpen, onClose }) {
+	const [title, setTitle] = useState(post.title);
+	const [content, setContent] = useState(post.content);
+	const onSubmit = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<Modal
+			isOpen={isOpen}
+			onRequestClose={onClose}
+			className={styles['new-post-modal']}
+			contentLabel="New Post Modal"
+		>
+			<h2>Editing "{title}"</h2>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					className={styles['post-title']}
+					value={title}
+					onChange={(e) => {
+						setTitle(e.target.value);
+					}}
+				/>
+				<br />
+				<br />
+				<textarea
+					value={content}
+					onChange={(e) => {
+						setContent(e.target.value);
+					}}
+					placeholder={'Write new blog post...'}
+					className={styles['post-textarea']}
+				>
+					{content}
+				</textarea>
+				<br />
+				<br />
+				<button type={'submit'} className={styles['post-button']}>
+					Save
+				</button>
+			</form>
+			<button
+				type={'button'}
+				onClick={onClose}
+				className={styles['cancel-button']}
+			>
+				Cancel
+			</button>
+		</Modal>
 	);
 }
 
--- src/services/Api.js
+++ src/services/Api.js
@@ -68,7 +68,7 @@ export class Api {
 		}
 	};
 
-    getMe = async () => {
+	getMe = async () => {
 		const url = `${this.host}/me`;
 		try {
 			const { data } = await axios.get(url, {
@@ -77,8 +77,8 @@ export class Api {
 				},
 			});
 
-			let { employee } = data;
-			return employee;
+			let { me } = data;
+			return me[0];
 		} catch (e) {
 			if (e.response?.data?.message) {
 				throw new Error(e.response?.data.message);
@@ -88,7 +88,7 @@ export class Api {
 		}
 	};
 
-    getCategories = async () => {
+	getCategories = async () => {
 		const url = `${this.host}/categories`;
 		try {
 			const { data } = await axios.get(url, {
@@ -108,8 +108,8 @@ export class Api {
 		}
 	};
 
-    getPostsFromCategory = async (id) => {
-        const url = `${this.host}/categories/${id}`;
+	getPostsFromCategory = async (id) => {
+		const url = `${this.host}/categories/${id}`;
 		try {
 			const { data } = await axios.get(url, {
 				headers: {
@@ -126,5 +126,110 @@ export class Api {
 
 			throw new Error(e.message);
 		}
+	};
+
+	createPost = async ({ title, description, content, categoryId }) => {
+		const url = `${this.host}/categories/${categoryId}`;
+		const body = {
+			title,
+			description,
+			content,
+		};
+		try {
+			const { data } = await axios.post(url, body, {
+				headers: {
+					authorization: `Bearer ${this.token}`,
+				},
+			});
+			return data.id;
+		} catch (e) {
+			if (e.response?.data?.message) {
+				throw new Error(e.response?.data.message);
+			}
+
+			throw new Error(e.message);
+		}
+	};
+
+    getPost = async (id) => {
+        const url = `${this.host}/posts/${id}`;
+		try {
+			const { data } = await axios.get(url, {
+				headers: {
+					authorization: `Bearer ${this.token}`,
+				},
+			});
+
+			let { post } = data;
+			return post;
+		} catch (e) {
+			if (e.response?.data?.message) {
+				throw new Error(e.response?.data.message);
+			}
+
+			throw new Error(e.message);
+		}
     }
+
+    deletePost = async (id) => {
+        const url = `${this.host}/posts/${id}`;
+		try {
+			const { data } = await axios.delete(url, {
+				headers: {
+					authorization: `Bearer ${this.token}`
+				},
+			});
+			const { id } = data;
+			return id;
+		} catch (e) {
+			if (e.response?.data?.message) {
+				throw new Error(e.response?.data.message);
+			}
+
+			throw new Error(e.message);
+		}
+    }
+
+    addComment = async ({ postId, content }) => {
+		const url = `${this.host}/comments`;
+		const body = {
+			postId,
+			content,
+		};
+		try {
+			const { data } = await axios.post(url, body, {
+				headers: {
+					authorization: `Bearer ${this.token}`,
+				},
+			});
+			return data.id;
+		} catch (e) {
+			if (e.response?.data?.message) {
+				throw new Error(e.response?.data.message);
+			}
+
+			throw new Error(e.message);
+		}
+	};
+
+    deleteComment = async (id) => {
+        const url = `${this.host}/comments/${id}`;
+		try {
+			const { data } = await axios.delete(url, {
+				headers: {
+					authorization: `Bearer ${this.token}`
+				},
+			});
+			const { id } = data;
+			return id;
+		} catch (e) {
+			if (e.response?.data?.message) {
+				throw new Error(e.response?.data.message);
+			}
+
+			throw new Error(e.message);
+		}
+    }
+
+
 }
